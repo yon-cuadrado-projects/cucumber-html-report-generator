1c004e92910a5ab891915d7fac1f5af8b742bfb3
Yon <yon.cuadrado@gmail.com>
2022-10-16 20:10:08 +0200

    updated selenium

--- test/e2e/configuration/base-config.ts
+++ test/e2e/configuration/base-config.ts
@@ -107,7 +107,7 @@ const config: WebdriverIO.Config = {
       {
         logPath: './.tmp/',
         installArgs: {
-          version: '4.4.0',
+          version: '4.5.0',
           drivers: {
             chrome: {
               version: 'latest',
